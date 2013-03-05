8feabe7fe0a2a6f6837203d0c708738886449cd1
Tuomas Aaltonen <tacitus@outlook.com>
2013-03-05 18:04:56 +0200

    v0.0.1
    testi

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+void Menu();
+void InputShips();
+void PlayGame();
+void Shoot();
+void RandShips();
+void SaveLog();
+void Spyplane();
+void AnnoyPlayer();
+void Secretary();
+void Config();
+//cmd.exe linewidth: 80 chars
+void SaveLoad();
+void MachineCommander();
+void ReadConfig();
+
+int Main()
+{
+	srand(time(0));
+	//calls here
+	return 0;
+}
